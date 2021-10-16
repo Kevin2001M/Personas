8d38aca4b64cd78c802a16e11474df5c2d5783f6
Kevin2001M <78760245+Kevin2001M@users.noreply.github.com>
2021-10-15 19:23:44 -0600

    Trabajando punto 1,2,3,4,5

---
+++ LaboratorioN2/LaboratorioN2/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using LaboratorioN2.Dominio;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaboratorioN2.Data
+{
+    public class ApplicationDbContext : DbContext
+    { 
+        
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                 : base(options)
+            {
+
+            }
+
+            public DbSet<persona> Students { get; set; }
+        }
+    
+}
---
+++ LaboratorioN2/LaboratorioN2/Dominio/persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaboratorioN2.Dominio
+{
+    public class persona
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPersona { get; set; }
+
+
+        public string NombrePersona { get; set; }
+
+        public int EdadPersona { get; set; }
+
+        public string DescripcionPersona { get; set; }
+
+        public ICollection<persona> personas { get; set; }
+    }
+}
