403bd974bb8612c73f94d40051ab7d6e2d331b12
Kevin2001M <78760245+Kevin2001M@users.noreply.github.com>
2021-10-16 18:40:31 -0600

    Update Validations

--- LaboratorioN2/LaboratorioN2/Dominio/PersonaViewModel.cs
+++ LaboratorioN2/LaboratorioN2/Dominio/PersonaViewModel.cs
@@ -4,17 +4,17 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LaboratorioN2.Models.PersonaViewModels
+namespace LaboratorioN2.Dominio
 {
     public class PersonaViewModel
     {
-        [Display(Name ="Nombre")]
+        [Display(Name = "Nombre")]
         [Required(ErrorMessage = "DATO REQUERIDO")]
         public string Nombre { get; set; }
 
-        [Display (Name ="Edad")]
+        [Display(Name = "Edad")]
         [Required(ErrorMessage = "DATO REQUERIDO")]
-        [Range(18,int.MaxValue, ErrorMessage = "LO SIENTO ERES MENOR DE EDAD")]
+        [Range(18, int.MaxValue, ErrorMessage = "LO SIENTO ERES MENOR DE EDAD")]
         public int Edad { get; set; }
 
         [Display(Name = "Descripcion")]
